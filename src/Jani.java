0def199c67c436177920fe7d6619edfe87df0cd0
Przemek B <pprzemek89@gmail.com>
2024-03-14 13:22:01 +0100

    zmiana 14.03

---
+++ src/Jani.java
@@ -0,0 +1,10 @@
+public class Jani implements Uczen{
+    @Override
+    public void uczSie() {
+        System.out.println("ucze się bo monka mnie zmusza");
+    }
+    public void ogladajKiepskich(){
+        System.out.println("ioio");
+    }
+
+}
---
+++ src/Krystainek.java
@@ -0,0 +1,9 @@
+public class Krystainek implements Uczen {
+    @Override
+    public void uczSie() {
+        System.out.println("Ioio");
+    }
+    public void grajWKulki(){
+        System.out.println("ciecierecie");
+    }
+}
